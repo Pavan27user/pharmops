3ad5ed4ad0b88061ea44e3d4a9c5204b7de08d95
Pavan27user <pavangowda279m@gmail.com>
2026-06-01 13:48:49 +0000

    BUILD INFRA

--- pharma-devops/terraform/envs/dev/backend.tf
+++ pharma-devops/terraform/envs/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "pharma-tf-state-loki-1"
+    bucket         = "pharma-tf-state-loki-27"
     key            = "envs/dev/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "pharma-tf-lock-loki"
+    dynamodb_table = "pharma-tf-lock-loki-27"
     encrypt        = true
   }
 }
